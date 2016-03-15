08ef389cd878351f4aa4943a78789b440008600d
Aaron Ash <aaron.ash@gmail.com>
2016-03-15 14:45:06 +0700

    Initial script rewrite

--- vimtohtml.sh
+++ vimtohtml.sh
@@ -1,6 +1,66 @@
 #!/bin/bash
 
-
-
 # AA TODO: currently this in an alias so you have to source this script or put it in your .bashrc
-alias vimtohtml='vim -c ":argdo set eventignore-=Syntax | if &filetype != \"\" | TOhtml | endif | wq" -c "wqa"'
+#alias vimtohtml='vim -c ":argdo set eventignore-=Syntax | if &filetype != \"\" | TOhtml | endif | wq" -c "wqa"'
+
+set -e
+
+## Render a whole directory out to HTML with vim's :TOhtml command
+## Usage: vimtohtml.sh [options]
+##
+##       [-h     | --help]     Print this help message
+##       [-c colorscheme | --colorscheme colorscheme]  Use the specified colourscheme
+##       [-b arg | --bar arg]  Bars the baz
+
+BASE=$(cd $(dirname $0); pwd -P)
+
+usage() {
+   echo "$(grep "^## " "${BASH_SOURCE[0]}" | cut -c 4-)"
+   exit 0
+}
+
+error() {
+   cat <<< "$@" 1>&2
+   exit 1
+}
+
+[[ $# == 0 ]] && usage
+
+# Transform long options to short ones
+for arg in "$@"; do
+  shift
+  case "$arg" in
+    "--help") set -- "$@" "-h" ;;
+    "--colorscheme")  set -- "$@" "-c" ;;
+    "--bar")  set -- "$@" "-b" ;;
+    *)        set -- "$@" "$arg"
+  esac
+done
+# Parse short options
+OPTIND=1
+while getopts "hc:b:" opt
+do
+  case "$opt" in
+    "h") usage; exit 0 ;;
+    "c") COLORSCHEME="-c "":colorscheme $OPTARG""" ;;
+    "b") BAR="$OPTARG" ;;
+    "?") usage >&2; exit 1 ;;
+    ":") error "Option -$OPTARG requires an argument.";;
+  esac
+done
+shift $(expr $OPTIND - 1) # remove options from positional parameters
+
+ARGDOCOMMAND='-c '':argdo set eventignore-=Syntax | if &filetype != "" | TOhtml | endif | wq'' -c "wqa"'
+
+ARGS=$@
+
+if [[ -n "$COLORSCHEME" ]]; then
+   echo "COLORSCHEME: $COLORSCHEME"
+fi
+if [[ -n "$BAR" ]]; then
+   echo "BAR: $BAR"
+fi
+
+echo "Rest of the args were: $ARGS"
+
+vim "$COLORSCHEME" -c ":argdo set eventignore-=Syntax | if &filetype != \"\" | TOhtml | endif | wq" -c "wqa" "$ARGS"
